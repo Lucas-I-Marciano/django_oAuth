8e6248ae967a61555abaa69b2b3a2683a91fe058
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-02 13:44:44 -0300

    :sparkles: feat: Adding setting for server serve static files

--- setup/settings.py
+++ setup/settings.py
@@ -119,6 +119,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Static assets that aren’t tied to a particular app
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'templates/static')
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
